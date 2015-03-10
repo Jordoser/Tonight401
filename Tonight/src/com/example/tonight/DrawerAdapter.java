d4d8d6100f245e6833247b538f59c63489ea1326
Jeremy Unger <jmunger@ualberta.ca>
2015-03-09 20:05:09 -0600

    Drawer has been separated into its own fragment. All functionality seems to be working except updating drawer on move to new area fragment.

---
+++ Tonight/src/com/example/tonight/DrawerAdapter.java
@@ -0,0 +1,99 @@
+package com.example.tonight;
+
+/**
+ * Created by junker4ce on 15-03-09.
+ */
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class DrawerAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<String> titles;
+//    private int[] images;
+    private LayoutInflater inflater;
+    private int[] selectedPosition;
+
+    /*public DrawerAdapter(Context context, ArrayList<String> titles, int[] images,
+                           int[] selectedPosition) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.titles = titles;
+        this.images = images;
+        this.inflater = LayoutInflater.from(this.context);
+        this.selectedPosition = selectedPosition;
+    }*/
+
+    public DrawerAdapter(Context context, ArrayList<String> titles,
+                           int[] selectedPosition) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.titles = titles;
+        this.inflater = LayoutInflater.from(this.context);
+        this.selectedPosition = selectedPosition;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return titles.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+
+        ViewHolder mViewHolder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.drawer_list, null);
+            mViewHolder = new ViewHolder();
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        mViewHolder.tvTitle = (TextView) convertView
+                .findViewById(R.id.barText);
+        mViewHolder.ivIcon = (ImageView) convertView
+                .findViewById(R.id.barLogo);
+
+        mViewHolder.tvTitle.setText(titles.get(position));
+        //mViewHolder.ivIcon.setImageResource(images[position]);
+
+        //Highlight the selected list item
+        if (position == selectedPosition[0]) {
+            convertView.setBackgroundColor(Color.WHITE);
+            mViewHolder.tvTitle.setTextColor(Color.BLUE);
+        } else {
+            convertView.setBackgroundColor(Color.TRANSPARENT);
+            mViewHolder.tvTitle.setTextColor(Color.WHITE);
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView tvTitle;
+        ImageView ivIcon;
+    }
+}
--- Tonight/src/com/example/tonight/MainActivity.java
+++ Tonight/src/com/example/tonight/MainActivity.java
@@ -1,56 +1,42 @@
 package com.example.tonight;
 
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.support.v4.app.FragmentActivity;
-import android.app.ActionBar;
-import android.widget.TextView;
+import android.support.v4.widget.DrawerLayout;
+
 
 import com.example.tonight.adapter.TabsPagerAdapter;
-
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-
-//Look at https://developer.android.com/training/implementing-navigation/nav-drawer.html
+import com.example.tonight.fragment.DrawerFragment;
 
 public class MainActivity extends FragmentActivity implements
-        ActionBar.TabListener {
-    //private String[] mVenueNames;
-    private ArrayList<String> mVenueNames;
-    private ArrayList<String> mVenueIds;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private ArrayAdapter<String> mListAdapter;
-    private boolean mRunning;
-    Handler mHandler = new Handler();
+        ActionBar.TabListener, DrawerFragment.NavigationDrawerCallbacks  {
 
-    // Tab titles
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the
+     * navigation drawer.
+     */
+    private DrawerFragment mDrawerFragment;
+
+    private CharSequence mTitle;
+
+    private boolean mRunning;
+
     private String[] tabs = { "All", "Downtown", "South", "West", "Whyte" };
     private ViewPager viewPager;
     ActionBar actionBar;
     private TabsPagerAdapter mAdapter;
+    Handler mHandler = new Handler();
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
@@ -59,43 +45,21 @@ public class MainActivity extends FragmentActivity implements
         viewPager = (ViewPager) findViewById(R.id.pager);
         actionBar = getActionBar();
         mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
-        //getActionBar().show();
 
         viewPager.setAdapter(mAdapter);
         actionBar.setHomeButtonEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        mVenueNames = new ArrayList<String>(VenueListController.returnVenues());
-        mVenueIds = new ArrayList<String>(VenueListController.returnVenueIds());
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerList.setBackgroundColor(getResources().getColor(R.color.white));
-
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                //getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu();
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                //getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu();
-            }
-        };
-
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
 
-        mListAdapter = new ArrayAdapter<String>(this, R.layout.drawer_list_item, mVenueNames);
-        mDrawerList.setAdapter(mListAdapter);
-        mDrawerList.setSelector(R.drawable.list_selector);
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerFragment = (DrawerFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mDrawerFragment.setUp(R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
 
         // Adding Tabs
         for (String tab_name : tabs) {
@@ -122,74 +86,70 @@ public class MainActivity extends FragmentActivity implements
         }
     }
 
-    public void updateDrawer() {
-        System.out.println("Updated Drawer");
-        mVenueNames.clear();
-        mVenueNames.addAll(VenueListController.returnVenues());
-        mVenueIds.clear();
-        mVenueIds.addAll(VenueListController.returnVenueIds());
-        mListAdapter.notifyDataSetChanged();
-        mDrawerList.invalidate();
+    @Override
+    public void onDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        /*if (position == 0) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, HomeFragment.newInstance())
+                    .commit();
+        } else if (position == 1) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, MailFragment.newInstance())
+                    .commit();
+        } else if (position == 2) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, SettingsFragment.newInstance())
+                    .commit();
+        }*/
     }
 
-    public void updateDrawer(String area) {
-        System.out.println("Updated Drawer from Area");
-        mVenueNames.clear();
-        mVenueNames.addAll(VenueListController.returnVenues(area));
-        mVenueIds.clear();
-        mVenueIds.addAll(VenueListController.returnVenueIds(area));
-        mListAdapter.notifyDataSetChanged();
-        mDrawerList.invalidate();
-    }
-
-    //Drawer OnClick listener
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            final int pos = position;
-            final TextView tv = (TextView) view;
-            tv.setTextColor(getResources().getColor(R.color.white));
-
-            ParseOperations.getName(mVenueIds.get(position));
-
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    tv.setTextColor(getResources().getColor(R.color.pink));
-                    Intent intent = new Intent(MainActivity.this, VenueActivity.class);
-                    intent.putExtra("venue_id", mVenueIds.get(pos));
-                    startActivity(intent);
-                }
-            }, 750);
-
+   /* //Get the title of current fragment
+    public void onSectionAttached(int number) {
+        System.out.println(mTitle);
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
         }
-    }
+    }*/
 
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
+    /*//Change the title of page to current fragments title
+    public void restoreActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }*/
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            //restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
             return true;
-        }
-        else if(item.getItemId()==(R.id.userName)){
-            ScreenName.alert(this);
         }
         return super.onOptionsItemSelected(item);
     }
@@ -201,9 +161,10 @@ public class MainActivity extends FragmentActivity implements
         System.out.println("Selected Tab");
         System.out.println(tab.getText());
         if (tab.getText().equals("All")){
-            updateDrawer();
+            //updateDrawer();
         }
-        else {updateDrawer((String)tab.getText());}
+        else {//updateDrawer((String)tab.getText());
+        }
         viewPager.setCurrentItem(tab.getPosition());
     }
 
@@ -217,25 +178,13 @@ public class MainActivity extends FragmentActivity implements
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    public void refreshAll(){
-        mDrawerList.invalidate();
-    }
-
     Runnable mUpdater = new Runnable() {
         @Override
         public void run() {
             // check if still in focus
             if (!mRunning) return;
 
-            updateDrawer();
+            //updateDrawer();
 
             // schedule next run
             mHandler.postDelayed(this, 500); // set time here to refresh views
@@ -255,5 +204,4 @@ public class MainActivity extends FragmentActivity implements
         super.onPause();
         mRunning= false;
     }
-
 }
